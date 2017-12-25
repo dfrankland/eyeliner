35bf4f974eb8f80f8cde623367dbd5df946fd2cb
Dylan Frankland <dylan@frankland.io>
2017-12-25 02:17:31 -0800

    Add benchmark

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
+#![feature(test)]
+
 extern crate kuchiki;
 extern crate servo_css_parser;
 #[macro_use] extern crate markup5ever;
+extern crate test;
 
 pub mod eyeliner;
 
 use eyeliner::{Eyeliner, InlineStylesheetAndDocument};
+
+use test::Bencher;
 
 pub fn inline<'a>(html: &'a str, css: &'a str) -> String {
     let eyeliner = Eyeliner::new(html, css);
@@ -64,4 +69,40 @@ fn test() {
         expected_result_vec.join(""),
         result_vec.join(""),
     );
+}
+
+#[bench]
+fn bench(b: &mut Bencher) {
+    let html = r#"
+        <html>
+            <head>
+                <title>Hello, world!</title>
+            </head>
+            <body>
+                <h1>Hello, world!</h1>
+                <p class="foo bar">I love HTML</p>
+                <heart>&lt;3</heart>
+            </body>
+        </html>
+    "#;
+
+    let css = r#"
+        .foo {
+            color: black;
+        }
+        .foo.bar, heart {
+            color: red;
+            font-weight: bold;
+        }
+        .foo.bar {
+            text-decoration: underline !important;
+        }
+        .foo.bar {
+            text-decoration: inherit;
+        }
+    "#;
+
+    b.iter(|| {
+        test::black_box(inline(html, css));
+    });
 }
