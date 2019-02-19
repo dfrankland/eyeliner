2a95d9c9f24f9e4491c93301a3041ba677043456
Dylan Frankland <dylan@frankland.io>
2019-02-19 08:31:43 -0800

    Fix benches by setting embedder traits

--- benches/fast.rs
+++ benches/fast.rs
@@ -1,13 +1,14 @@
 #![feature(test)]
 
 extern crate test;
-extern crate eyeliner;
 
 use test::Bencher;
 use eyeliner::inline;
+use servo_css_parser::embedder_traits;
 
 #[bench]
 fn bench_dashboard(b: &mut Bencher) {
+    embedder_traits::resources::set_for_tests();
     let html = include_str!("./bootstrap/site/docs/4.3/examples/dashboard/index.html");
     let css = include_str!("./bootstrap/dist/css/bootstrap.css");
     b.iter(|| inline(html, Some(css.to_owned()), None, None))
@@ -15,6 +16,7 @@ fn bench_dashboard(b: &mut Bencher) {
 
 #[bench]
 fn bench_navbar(b: &mut Bencher) {
+    embedder_traits::resources::set_for_tests();
     let html = include_str!("./bootstrap/site/docs/4.3/examples/navbars/index.html");
     let css = include_str!("./bootstrap/dist/css/bootstrap.css");
     b.iter(|| inline(html, Some(css.to_owned()), None, None))
