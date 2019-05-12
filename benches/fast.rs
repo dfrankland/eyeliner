6837ac6aac1bb002067a97cb6bcb287fe6391f3c
Dylan Frankland <dylan@frankland.io>
2019-05-12 15:42:29 -0700

    Update bootstrap

--- benches/fast.rs
+++ benches/fast.rs
@@ -9,7 +9,7 @@ use test::Bencher;
 #[bench]
 fn bench_dashboard(b: &mut Bencher) {
     embedder_traits::resources::set_for_tests();
-    let html = include_str!("./bootstrap/site/docs/4.3/examples/dashboard/index.html");
+    let html = include_str!("./bootstrap/site/content/docs/4.3/examples/dashboard/index.html");
     let css = include_str!("./bootstrap/dist/css/bootstrap.css");
     b.iter(|| inline(html, Some(css.to_owned()), None, None))
 }
@@ -17,7 +17,7 @@ fn bench_dashboard(b: &mut Bencher) {
 #[bench]
 fn bench_navbar(b: &mut Bencher) {
     embedder_traits::resources::set_for_tests();
-    let html = include_str!("./bootstrap/site/docs/4.3/examples/navbars/index.html");
+    let html = include_str!("./bootstrap/site/content/docs/4.3/examples/navbars/index.html");
     let css = include_str!("./bootstrap/dist/css/bootstrap.css");
     b.iter(|| inline(html, Some(css.to_owned()), None, None))
 }
